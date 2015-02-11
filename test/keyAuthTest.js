f4d6a10caf7e05b1d1fd4f6103fed590f9c4b736
Neil Lokare <neil.lokare@gmail.com>
2015-02-11 14:28:47 -0800

    (Test) Refactors keyAuthTest to use assert and mocha and sets up npm test command in package.json

--- test/keyAuthTest.js
+++ test/keyAuthTest.js
@@ -1,21 +1,32 @@
-var expect = require('chai').expect;
+var mocha = require('mocha');
+var assert = require('chai').assert;
 var keyAuth = require('../server/keyAuth/keyAuth');
 var keys = require('../security/keys/keys');
 
-var keyCheck = keyAuth.keyCheck;
-var generateJWK = keyAuth.generateJWK;
-
-expect(keyCheck).to.be.a('function'); //check that keyCheck is a function
-expect(generateJWK).to.be.a('function'); //check that generateJWK is a function
-
-var genJWK = generateJWK('A1B1C1D1A2B2A3B3A4B4A5B5C5D5E5F5')
-  .then(function (jwk) {
-    expect(jwk).to.be.a('object'); //return object should be an object
-    expect(jwk.kty).to.eql('oct'); //key type should be an octet sequence
-    expect(jwk.alg).to.eql('A128GCM'); //algorithm should be the AES key wrap using a 128-bit key
-    expect(jwk.kid).to.eql('obHB0aKyo7OktKW1xdXl9Q'); //kid should be the base64 hexidecimal represenation of the keyid
-    expect(jwk.k).to.eql('JTn6hLmHQWAJp/u6EbI5qw'); //the key should match 'JTn6hLmHQWAJp/u6EbI5qw'
+describe('JWK Generation Functions', function () {
+  describe('Key Check', function () {
+    it('should return true when keyCheck is a function', function () {
+      var keyCheck = keyAuth.keyCheck;
+      assert.equal(typeof keyCheck, 'function'); //check that keyCheck is a function
+    });
   });
+  describe('JWK token creation', function () {
+    it('should return true when generateJWK is a function', function () {
+      var generateJWK = keyAuth.generateJWK;
+      assert.equal(typeof generateJWK, 'function'); //check that generateJWK is a function
+    });
+    it('should return a JSON Web Key', function () {
+      var genJWK = keyAuth.generateJWK('A1B1C1D1A2B2A3B3A4B4A5B5C5D5E5F5')
+        .then(function (jwk) {
+          assert.equal(typeof jwk, 'object'); //return object should be an object
+          assert.equal(jwk.kty, 'oct'); //key type should be an octet sequence
+          assert.equal(jwk.alg, 'A128GCM'); //algorithm should be the AES key wrap using a 128-bit key
+          assert.equal(jwk.kid, 'obHB0aKyo7OktKW1xdXl9Q'); //kid should be the base64 hexidecimal represenation of the keyid
+          assert.equal(jwk.k, 'JTn6hLmHQWAJp/u6EbI5qw'); //the key should match 'JTn6hLmHQWAJp/u6EbI5qw'
+        });
+    });
+  });
+});
 
 //write HTTP tests
 
